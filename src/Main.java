2ad8a5232aa1e7f802ef1cb1e73da68ad46e6490
KainSt <ksfgnw@gmail.com>
2022-01-20 23:51:58 +0300

    Работа с файлами

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args){
         File file = new File("/data/info.txt");
-        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getName());
     }
 
 
