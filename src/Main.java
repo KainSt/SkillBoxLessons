0b54da8957ae9e5f37aa2d0678a28602cf7dbae2
KainSt <ksfgnw@gmail.com>
2022-01-22 22:06:25 +0300

    Работа с файлами

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args){
         File file = new File("/data/info.txt");
         System.out.println(file.getName());
+        System.out.println(file.isDirectory());
+        System.out.println(file.getName());
     }
 
 
