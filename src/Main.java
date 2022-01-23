9a451a080682f3a46ee6936a7ce630fc9f376d09
KainSt <ksfgnw@gmail.com>
2022-01-23 23:15:57 +0300

    Работа с файлами

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,53 @@
 import java.io.File;
 
 public class Main {
-    public static void main(String[] args){
-        File file = new File("/data/info.txt");
-        System.out.println(file.getName());
-        System.out.println(file.isDirectory());
+
+    public static void main(String[] args) {
+        try{
+            File folder = new File("C:/Users/KS/Documents");
+            System.out.println(  convertLength( directorySize(folder) ) );
+        } catch (NullPointerException exception){
+            System.out.println(exception.getStackTrace());
+        }
+
+
+
+
 
     }
 
+    public static long directorySize(File folder){
+
+        if (!folder.isDirectory()) {
+            return folder.length();
+        } else {
+            File[] fileArray = folder.listFiles();
+            long currentSize = 0;
+            for (File element : fileArray) {
+                currentSize+= directorySize(element);
+           }
+            return currentSize;
+        }
+
+
+    }
+
+    public static String convertLength(long length) {
+
+        if (length < 1024) {
+            return length + " " + "Б";
+        }
+        if (length >= Math.pow(2, 10) & length < Math.pow(2, 20)) {
+            return String.format("%.2f",length / Math.pow(2, 10)) + " " + "КБ";
+        }
+
+        if (length >= Math.pow(2, 20) & length < Math.pow(2, 30)) {
+            return String.format("%.2f",length / Math.pow(2, 20)) + " " + "Мб";
+        }
+
+        return String.format("%.2f",length / Math.pow(2, 30) )+ " " + "Гб";
+
+
+    }
 
 }
